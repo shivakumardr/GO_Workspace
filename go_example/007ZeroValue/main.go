b4d3f760d18571ad8a585152d935fe3c4cd32da8
Shivakumar D R <shivakumara.dr@nichi.com>
2021-08-29 14:28:13 +0530

    Doing grate

---
+++ go_example/007ZeroValue/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+/*
+understanding zero value
+false for booleans
+0 for integers
+0.0 for floats
+"" for strings
+nil for
+pointers
+functions
+interfaces
+slices
+channels
+maps
+use short declaration operator as much as possible
+use var for
+zero value
+package scope
+
+*/
+var z string
+var x int
+var y float32
+var xx complex64
+func main(){
+	fmt.Println(z)
+	fmt.Printf("%T\n",z)
+	fmt.Println(x)
+	fmt.Printf("%T\n",x)
+	fmt.Println(y)
+	fmt.Printf("%T\n",y)
+	fmt.Println(xx)
+	fmt.Printf("%T\n",xx)
+}
