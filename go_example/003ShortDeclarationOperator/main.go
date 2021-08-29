782388cf213e2161906e66d5e3ed6d3dc9502437
Shivakumar D R <shivakumara.dr@nichi.com>
2021-08-29 05:40:08 +0530

    Short declaration and operator and experssion inrduction

---
+++ go_example/003ShortDeclarationOperator/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 40
+	fmt.Println(x)
+	x = 90
+	fmt.Println(x)
+	y := 120 + 30
+	fmt.Println(y)
+	strg := "good morning all"
+	n, errn := fmt.Println(strg)
+	fmt.Println(n, errn)
+	//	z:=80
+	//	__z
+
+}
