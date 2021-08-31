6e28fbbb77d44621f4402d7cf6bb1e60cffbfd21
Shivakumar D R <shivakumara.dr@nichi.com>
2021-08-31 05:37:02 +0530

    Bool Type

---
+++ go_example/010BoolType/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var x bool
+func main(){
+	fmt.Println(x)
+	x = true
+	fmt.Println(x)
+	a:= 7
+	b:= 32
+	c:=7
+	fmt.Println(a==b)
+	fmt.Println(a==c)
+	fmt.Println(a>=b)
+	fmt.Println(a<=b)
+	fmt.Println(c>=b)
+	fmt.Println(c>=a)
+
+
+}
