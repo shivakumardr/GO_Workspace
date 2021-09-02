2f1d185643c3f86dded7a656a7110c94a629a46a
Shivakumar D R <shivakumara.dr@nichi.com>
2021-09-02 14:32:25 +0530

    string and iota

---
+++ go_example/012String1/main.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main(){
+	s:= "Wlecome to go Strings"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+	bs:= []byte(s)
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
+
+	// Print UTF8 code
+	for i:=0; i<len(s); i++{
+		fmt.Printf("s[i] %v, = %#U\n",s[i], s[i])
+	}
+	for i, v:= range s{
+		fmt.Printf("\nIndex of %v, is having hex value %#x", i, v)
+
+	}
+
+}
